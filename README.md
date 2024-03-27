2c07daa0f548e4a0bb3022ab0ab1c05af107a7f3
gaurab-khanal <khanalgaurab98@gmail.com>
2024-03-27 15:15:45 +0545

    room lobby

--- README.md
+++ README.md
@@ -6,3 +6,26 @@
 - Express
 - WebRTC
 - SocketIo# meetUp
+
+
+## Setup
+
+ ### Client Setup
+    
+    ```
+        cd client
+        npm i
+        npm i -g peer
+        npm run dev
+        peerjs --port 3001 // in another terminal
+    ```
+
+
+
+### Server setup
+
+    ```
+    cd server
+    npm i 
+    npm run dev
+    ```
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,3 +1,57 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 export default function Home() {
-  return <>hello world</>;
+  
+  const router = useRouter();
+  const [roomId, setRoomId] = useState("")
+  const [error, setError] = useState("")
+
+  const joinRoom = () => {
+
+    if(!roomId) {
+      setError("Please enter a room id")
+      return;
+    }
+    router.push(`/room/${roomId}`);
+  }
+
+  const createAndJoin = () => {
+    const roomId = uuidv4();
+    router.push(`/room/${roomId}`);
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-center  h-screen   gap-4">
+        <div className="flex flex-col items-center gap-4">
+        <h1 className="text-xl ">Vid Meet App</h1>
+        <div className="flex flex-col items-center gap-2">
+          <div>
+            <input
+              type="text"
+              placeholder="Enter room id"
+              className="border-2 border-black pl-2 rounded-md mr-2"
+              value={roomId}
+              required
+              onChange={(e)=>setRoomId(e.target.value)}
+            />
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded-full" onClick={joinRoom}>
+              Join
+            </button>
+          </div>
+          {
+            error && <div className="text-red-500">{error}</div>
+          }
+          <span>OR</span>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={createAndJoin}>
+            Create a new room{" "}
+          </button>
+        </div>
+        </div>
+      </div>
+    </>
+  );
 }
